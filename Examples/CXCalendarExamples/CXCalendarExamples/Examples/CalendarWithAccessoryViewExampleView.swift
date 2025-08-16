2a26617fe54d378daff9032a3273b06ce816b1ff
Cunqi Xiao <Cunqi@users.noreply.github.com>
2025-08-16 07:49:57 -0700

    Update CalendarWithAccessoryViewExampleView.swift
    Update example

--- Examples/CXCalendarExamples/CXCalendarExamples/Examples/CalendarWithAccessoryViewExampleView.swift
+++ Examples/CXCalendarExamples/CXCalendarExamples/Examples/CalendarWithAccessoryViewExampleView.swift
@@ -21,7 +21,7 @@ struct CalendarWithAccessoryViewExampleView: View {
                 .builder
                 .hPadding(.zero)
                 .layoutStrategy(.fixedHeight(32))
-                .accessoryView { date, _ in
+                .accessoryView { date in
                     AccessoryView(date: date)
                 }
                 .build()
@@ -37,52 +37,53 @@ struct CalendarWithAccessoryViewExampleView: View {
 
     // MARK: Private
 
-    @State private var viewModel = ViewModel()
+    @State private var viewModel = CalendarWithAccessoryViewExampleView.ViewModel()
 }
 
-// MARK: - AccessoryView
+// MARK: CalendarWithAccessoryViewExampleView.AccessoryView
 
-struct AccessoryView: View {
-    @Environment(CalendarWithAccessoryViewExampleView.ViewModel.self) var viewModel
+extension CalendarWithAccessoryViewExampleView {
+    struct AccessoryView: View {
+        @Environment(CalendarWithAccessoryViewExampleView.ViewModel.self) var viewModel
 
-    let date: Date
+        let date: Date
 
-    var items: [ActionItem] {
-        viewModel.makeItems(for: date)
-    }
-
-    var body: some View {
-        VStack {
-            Text(date, format: .dateTime.day().month(.wide))
-                .font(.body)
-                .bold()
-                .frame(maxWidth: .infinity)
-                .padding(.vertical, CXSpacing.oneX)
-                .background {
-                    RoundedRectangle(cornerRadius: 10.0)
-                        .fill(Color.secondarySystemGroupedBackground)
-                }
-                .padding(.horizontal)
-                .padding(.top)
-            List {
-                ForEach(items) { item in
-                    Text(item.title)
-                        .font(.body)
-                        .padding()
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .listRowInsets(EdgeInsets())
-                }
-            }
-            .cornerRadius(10.0)
-            .listStyle(.plain)
-            .scrollIndicators(.hidden)
-            .padding()
+        var items: [ActionItem] {
+            viewModel.makeItems(for: date)
         }
-        .background(Color.systemGroupedBackground)
+
+        var body: some View {
+            VStack {
+                Text(date, format: .dateTime.day().month(.wide))
+                    .font(.body)
+                    .bold()
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, CXSpacing.oneX)
+                    .background {
+                        RoundedRectangle(cornerRadius: 10.0)
+                            .fill(Color.secondarySystemGroupedBackground)
+                    }
+                    .padding(.horizontal)
+                    .padding(.top)
+                List {
+                    ForEach(items) { item in
+                        Text(item.title)
+                            .font(.body)
+                            .padding()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .listRowInsets(EdgeInsets())
+                    }
+                }
+                .cornerRadius(10.0)
+                .listStyle(.plain)
+                .scrollIndicators(.hidden)
+            }
+            .background(Color.systemGroupedBackground)
+        }
     }
 }
 
-// MARK: - CalendarWithAccessoryViewExampleView.ViewModel
+// MARK: CalendarWithAccessoryViewExampleView.ViewModel
 
 extension CalendarWithAccessoryViewExampleView {
     @Observable
